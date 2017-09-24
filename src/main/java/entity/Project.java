f1cced072d723326aca65c6632a83e58017d7527
ArtemYankovets <ArtemYankovets@gmail.com>
2017-09-24 23:46:53 +0300

    creating entities

---
+++ src/main/java/entity/Project.java
@@ -0,0 +1,58 @@
+package entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Set;
+
+@Entity
+@Table(name = "PROJECT")
+public class Project implements Serializable {
+
+    private static final long serialVersionUID = -1796556740932985128L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "TITLE")
+    private String title;
+
+    @ManyToMany(mappedBy = "projects")
+    private Set<Employee> employees;
+
+    public Project() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Set<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(Set<Employee> employees) {
+        this.employees = employees;
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                '}';
+    }
+}
